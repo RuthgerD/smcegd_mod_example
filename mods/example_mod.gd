20334797d178d794bc0f5807f6ee9c7082fe3fff
Ruthger Dijt <ruthgerdijt@gmail.com>
2021-05-15 00:10:10 +0200

    Add simple vehicle mod

--- mods/example_mod.gd
+++ mods/example_mod.gd
@@ -3,7 +3,6 @@ extends Reference
 var mod_name: String = "example"
 
 func init(global) -> void:
-	
 	global.register_environment("example/Example", preload("res://src/environments/example/Example.tscn"))
-	
+	global.register_vehicle("StupidCar", preload("res://src/vehicles/stupid_car/StupidCar.tscn"))
 	print("Hello World!")
